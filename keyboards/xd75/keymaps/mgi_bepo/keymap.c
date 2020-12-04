48c5372786be113382e5ccca186d4e7070e3d937
Manuel Giraud <manuel@ledu-giraud.fr>
2020-12-04 16:29:44 +0100

    [xd75/mgi_bepo] update copyright

--- keyboards/xd75/keymaps/mgi_bepo/keymap.c
+++ keyboards/xd75/keymaps/mgi_bepo/keymap.c
@@ -1,8 +1,8 @@
-/* Copyright 2018 Manuel Giraud
+/* Copyright 2018, 2020 Manuel Giraud
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
